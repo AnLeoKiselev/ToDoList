adb14f1686b305ae3ff03a0ac7846a293c9f2f66
AnLeoKiselev <anleokiselev@gmail.com>
2022-11-28 15:58:20 +0300

    custom cells!

--- ToDoList/TaskListViewController.swift
+++ ToDoList/TaskListViewController.swift
@@ -3,59 +3,85 @@
 //  ToDoList
 //
 //  Created by Anatoliy on 18.11.2022.
-//
+// https://www.youtube.com/watch?v=Pu7B7uEzP18
 
 import UIKit
 
 class TaskListViewController: UIViewController {
+    
+    private lazy var backGroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background2")
+        //imageView.alpha = 0.3
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    } ()
     
     private let users = ["Boris", "Anatoliy", "Islam", "Gennadiy", "Marina", "Faina", "Nadezhda"]
     
     private lazy var taskListTableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.backgroundColor = .white
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        //tableView.backgroundColor = .white
+        //tableView.dataSource = self
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
+        //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.layer.cornerRadius = 10
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Task List"
+        taskListTableView.dataSource = self
+        taskListTableView.delegate = self
         addToSubview()
         addConstraints()
     }
     
     private func addToSubview() {
+        view.addSubview(backGroundImageView)
         view.addSubview(taskListTableView)
     }
     
     private func addConstraints() {
+        
+        backGroundImageView.frame = view.bounds
+        
         NSLayoutConstraint.activate([
-            taskListTableView.topAnchor.constraint(equalTo: view.topAnchor),
-            taskListTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            taskListTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            taskListTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            taskListTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            taskListTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            taskListTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            taskListTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
         ])
     }
 }
 
-extension TaskListViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        2
-    }
+extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        2
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .gray
-        cell.textLabel?.text = users[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as? CustomTableViewCell else {
+            return UITableViewCell()
+        }
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //cell.backgroundColor = .gray
+        //cell.textLabel?.text = users[indexPath.row]
+        
+        cell.configure(text: users[indexPath.row], imageName: "bird")
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
 }
     // про табл UITableViewCell - создать массив задач магаз... в станд ячейку перенести - заголовок и описание - навернуть все что есть  - закруглиь, дизайн таблицы ...
